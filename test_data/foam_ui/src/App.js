207d2aa26de26e78ad9857ba2c1116fc6f12e0cc
Jesse Johnson <jejo.math@gmail.com>
2023-02-13 15:46:44 -0500

    Make the react library installable.

--- test_data/foam_ui/src/App.js
+++ test_data/foam_ui/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {schema, enums, pages} from './config.js';
-import AppRouter from './AppRouter';
+import { AppRouter } from './lib/index.js';
+import './index.css';
 import './App.css';
-import './foam.css';
+import './lib/foam.css';
 
 function App() {
     return (
+        <div>
+        <div>Hello!</div>
         <div className='page-div'>
             <div className='header-div'>[Your Name Here]</div>
             <div className='content-div'>
                 <AppRouter schema={schema} enums={enums} pages={pages} />
             </div>
-        </div>
+        </div></div>
     )
 }
 
---
+++ test_data/foam_ui/src/lib/index.js
@@ -0,0 +1,10 @@
+import AppRouter from './AppRouter';
+import RecordPage from './RecordPage.js';
+import TablePage from './TablePage.js';
+import LinksPage from './LinksPage.js';
+import FigurePage from './FigurePage.js';
+import LayoutPage from './LayoutPage.js';
+import LoginPage from './LoginPage.js';
+import { UnderConstruction } from './Components.js';
+
+export { AppRouter, RecordPage, TablePage, LinksPage, FigurePage, LayoutPage, LoginPage, UnderConstruction }
